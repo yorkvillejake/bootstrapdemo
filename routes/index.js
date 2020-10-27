1a4ef3f585d6cf84f9a68465e9e082d7872e1083
Jakob Zimmermann <jakob.zimmermann@gmail.com>
2020-10-27 08:56:09 -0500

    testing put from submit btn

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('It works!');
+  res.render('form', { title: 'Registration form' });
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.render('form', { title: 'Registration form' });
 });
 
 module.exports = router;
